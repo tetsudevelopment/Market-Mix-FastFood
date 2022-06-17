9d8c2a791d226202f4a5b1614372a432d372b759
tetsudevelopment <bresmacawork@gmail.com>
2022-06-17 14:31:47 -0500

    Modal carrito, tablas y ver pedido

--- main.js
+++ main.js
@@ -3,6 +3,27 @@ let app = new Vue({
   data: {
     cantida: 0,
     userId: "1234",
-    userPin:"1234"
+    userPin: "1234",
+    viewMain: 0,
+    viewEmployee:0,
+
+
+
+
+
+  },
+  methods: {
+    getOut() {
+      this.viewMain = 0;
+    },
+    login() {
+      this.viewMain = 1;
+    },
+    waiter() {
+      this.viewEmployee = 2;
+    },
+    cook() {
+      this.viewEmployee = 1;
+    }
   }
 })
