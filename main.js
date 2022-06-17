ff895209f4cbb407b0cc8ea04c6670a0b72c6dd6
Carlos Montoya <camontoya30@gmail.com>
2022-06-17 14:51:29 -0500

    Purchasing and administrative unification

--- main.js
+++ main.js
@@ -1,10 +1,29 @@
-var app = new Vue({
+let app = new Vue({
     el: '#app',
     data: {
-        
+      cantida: 0,
+      userId: "1234",
+      userPin: "1234",
+      viewMain: 0,
+      viewEmployee:0,
+  
+  
+  
+  
+  
     },
     methods: {
-        
+      getOut() {
+        this.viewMain = 0;
+      },
+      login() {
+        this.viewMain = 1;
+      },
+      waiter() {
+        this.viewEmployee = 2;
+      },
+      cook() {
+        this.viewEmployee = 1;
+      }
     }
-    
-})
+  })
